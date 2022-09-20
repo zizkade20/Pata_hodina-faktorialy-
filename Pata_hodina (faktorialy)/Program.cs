ccb765c5d5201a2d3c9e4cc40955a9cb81edbf88
Denis Žižka <zizkade20@sps-prosek.cz>
2022-09-20 16:18:35 +0200

    Přidejte soubory projektu.

---
+++ Pata_hodina (faktorialy)/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+Random rnd = new Random();
+int n = rnd.Next(1, 100);
+
+
+int soucet = 0;
+int faktorial;
+int odmocnina;
+int cifsou = 0;
+int jedna;
+int pozpatku;
+
+// SOUCET --------------
+for (int i = 1; i < n; i++)
+{
+    soucet += n;
+}
+
+
+
+Console.WriteLine(n);
+Console.WriteLine("součet: " + soucet);
+Console.WriteLine("faktoriál:");
+
+
+
+
+int suma = 0;
+while (n != 0)
+{
+    suma += n % 10;
+    n /= 10;
+}
+Console.WriteLine("ciferný součet: " + suma);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
